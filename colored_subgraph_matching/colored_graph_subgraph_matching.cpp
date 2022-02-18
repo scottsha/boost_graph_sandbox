9bb33f42a8b2e45afd09c18a1a5ce3a6a41441ca
scottsha <scottshaloo@gmail.com>
2022-02-18 02:49:18 -0500

    ensure connectedness

--- colored_subgraph_matching/colored_graph_subgraph_matching.cpp
+++ colored_subgraph_matching/colored_graph_subgraph_matching.cpp
@@ -5,11 +5,12 @@
 struct TransitiveSubgraphChecker {
     VertexMatchMap map_merged;
     VertexMatchMap map_conflicts;
-    bool is_subgraphs_compatible;
+    bool is_connected_and_compatible;
     TransitiveSubgraphChecker(const VertexMatchMap& map0_, const VertexMatchMap& map1_) {
         map_merged = map0_;
         map_conflicts = map1_;
         map_merged.merge(map_conflicts);
+        bool is_subgraphs_connect = !map_conflicts.empty();
         for (auto it = map_conflicts.begin(); it!= map_conflicts.end();) {
             auto mit = map_merged.find(it->first);
             if (mit != map_merged.end()) {
@@ -20,7 +21,8 @@ struct TransitiveSubgraphChecker {
             }
             ++it;
         }
-        is_subgraphs_compatible = map_conflicts.empty();
+        bool is_subgraphs_compatible = map_conflicts.empty();
+        is_connected_and_compatible = is_subgraphs_connect and is_subgraphs_compatible;
     }
 };
 
@@ -29,6 +31,7 @@ class CommonSubgraphCallback {
         const ColoredGraph &graph0_;
         const ColoredGraph &graph1_;
         std::vector<VertexMatchMap> match_maps_;
+        VertexMatchMap* last_match_ = nullptr;
 
     public:
         CommonSubgraphCallback(const ColoredGraph &graph_0,
@@ -57,25 +60,28 @@ class CommonSubgraphCallback {
             if (match_maps_.empty())
             {
                 match_maps_.push_back(match_map);
+                last_match_ = &match_maps_.back();
                 return true;
             }
             // Check if the matches are actually just redundant inclusions:
-            const VertexMatchMap& last_match = match_maps_.back();
             // If this is really depth first backtracking search, you should always encounter supergraphs if they get larger,
             // and you should always encounter an incomparable subgraph if you drop down in size or stay the same size,
             // but you might have encountered an earlier graph.
-            if (subgraph_size > last_match.size()) {
-                match_maps_.back() = match_map;
+            if (subgraph_size > last_match_->size()) {
+                *last_match_ = match_map;
                 return true;
             }
             for (auto& old_map : match_maps_) {
                 TransitiveSubgraphChecker checker(old_map, match_map);
-                if(checker.is_subgraphs_compatible){
+                if(checker.is_connected_and_compatible){
+                    // The subgraphs are compatible and connect, so merge them.
                     old_map = checker.map_merged;
+                    last_match_ = &old_map;
                     return (true);
                 }
             }
             match_maps_.push_back(match_map);
+            last_match_ = &match_maps_.back();
             return (true);
         }
 };
@@ -86,10 +92,10 @@ std::vector<VertexMatchMap> common_connected_subgraphs(ColoredGraph g0, ColoredG
             boost::get(boost::vertex_color, g0), boost::get(boost::vertex_color, g1));
     //
     CommonSubgraphCallback subgraph_callback(g0, g1);
-    boost::property_map<ColoredGraph , boost::vertex_index_t>::type g0_vertex_index_map = boost::get(boost::vertex_index, g0);
-    boost::property_map<ColoredGraph , boost::vertex_index_t>::type g1_vertex_index_map = boost::get(boost::vertex_index, g1);
+    boost::property_map<ColoredGraph, boost::vertex_index_t>::type g0_vertex_index_map = boost::get(boost::vertex_index, g0);
+    boost::property_map<ColoredGraph, boost::vertex_index_t>::type g1_vertex_index_map = boost::get(boost::vertex_index, g1);
     // Actual subgraph matching call:
-    boost::mcgregor_common_subgraphs_unique(
+    boost::mcgregor_common_subgraphs(
         g0,
         g1,
         g0_vertex_index_map,
